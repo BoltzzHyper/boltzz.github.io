79c33a1d76703cf5fe9d850b2a2a958b8c7286af
sanshz <anshsaini6261@gmail.com>
2023-04-02 15:44:28 +0530

    Added and Removed some files

--- assets/extend.js
+++
@@ -1,9 +0,0 @@
-// Select the elements we need
-const menuToggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('nav');
-
-// Add an event listener to the menu toggle button
-menuToggle.addEventListener('click', function() {
-  // Toggle the "open" class on the nav element
-  nav.classList.toggle('open');
-});
